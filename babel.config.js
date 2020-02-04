bca699a3a636d303158cfd9d410040473ea5f18c
zmmsdk <you@example.com>
2020-02-04 09:05:00 +0800

    然后将 babel.config.js 中修改为

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
+// 暴露
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  // 配置vant组件库
+  plugins: [
+    ['import', {
+      libraryName: 'vant',
+      libraryDirectory: 'es',
+      style: true
+    }, 'vant']
   ]
+
 }
